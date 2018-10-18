6aee067b704d1356527bcd047fa28ba75ddf02be
rccm071994 <rccm071994@gmail.com>
2018-10-18 11:01:35 -0400

    Aver k pasa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Crud Laravel 5.4 2018
+                    Crud Laravel 5.4 2018 Ian Cota
                 </div>
 
                 <div class="links">
